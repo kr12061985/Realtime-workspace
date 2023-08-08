418aec37d7973e0948d351c72e3a429bfca68f7c
kr12061985 <92028493+kr12061985@users.noreply.github.com>
2023-08-08 17:01:19 +0530

    Update SpringBootSpringBatchSchedulerJobsApp.java
    2nd commit

--- spring-boot-spring-batch-scheduler-jobs-master/src/main/java/com/techgeeknext/spring/batch/scheduler/SpringBootSpringBatchSchedulerJobsApp.java
+++ spring-boot-spring-batch-scheduler-jobs-master/src/main/java/com/techgeeknext/spring/batch/scheduler/SpringBootSpringBatchSchedulerJobsApp.java
@@ -9,6 +9,7 @@ public class SpringBootSpringBatchSchedulerJobsApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSpringBatchSchedulerJobsApp.class, args);
+		System.out.println("Kishore");
 	}
 
 }
